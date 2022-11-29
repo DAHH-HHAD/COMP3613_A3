4217cfd10ce88922ec95b00df8911cfcd5205752
HariBarran <62348567+HariBarran@users.noreply.github.com>
2022-11-29 11:30:15 -0400

    Update config.py

--- App/config.py
+++ App/config.py
@@ -1,4 +1,4 @@
-SQLALCHEMY_DATABASE_URI = "postgresql://distnzfhejqvna:f1faaf59b02fe9d0907996b12f4eaaf285353fce9771f575fea29055c399b588@ec2-44-205-41-76.compute-1.amazonaws.com:5432/d1go96mnarl8o9"
+SQLALCHEMY_DATABASE_URI = "sqlite:///temp-database.db"
 SECRET_KEY = "secret"
 JWT_EXPIRATION_DELTA = 7
 ENV = "DEVELOPMENT"
